22a875cf2e7d5f3d6453fab8557b9b7f36330c9f
Gman6680 <143504495+Gman6680@users.noreply.github.com>
2024-03-28 10:48:30 +0000

    Merge branch 'main' of https://github.com/Gman6680/Learning-platform-project
    henlo

---
+++ public/lesson.js
@@ -0,0 +1,31 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+});
